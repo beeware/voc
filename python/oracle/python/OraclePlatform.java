f07cad61cf7de644c5a1bff2b36239a6f7ed7e41
Russell Keith-Magee <russell@keith-magee.com>
2015-10-07 14:15:37 +0800

    Added implementation of Android platform module.

--- python/oracle/python/OraclePlatform.java
+++ python/oracle/python/OraclePlatform.java
@@ -1,10 +1,14 @@
 package python;
 
+import java.lang.management.ManagementFactory;
+import java.lang.management.ThreadMXBean;
+
+
 class OraclePlatform implements python.PythonPlatform {
     public OraclePlatform() {}
 
     public long clock() {
-        java.lang.management.ThreadMXBean tmxb = java.lang.management.ManagementFactory.getThreadMXBean();
+        ThreadMXBean tmxb = ManagementFactory.getThreadMXBean();
         return tmxb.getCurrentThreadCpuTime();
     }
 
