dfea54bf4b93d3363401f24c59eded1481aa8b55
Russell Keith-Magee <russell@keith-magee.com>
2015-11-18 11:40:08 +0800

    Corrected error handling in Constructors.

--- python/common/org/python/types/Constructor.java
+++ python/common/org/python/types/Constructor.java
@@ -20,12 +20,13 @@ public class Constructor extends org.python.types.Object implements org.python.C
             throw new org.python.exceptions.RuntimeError("Illegal access to Java constructor " + this.value);
         } catch (java.lang.reflect.InvocationTargetException e) {
             try {
+                // e.getTargetException().printStackTrace();
                 // If the Java method raised an Python exception, re-raise that
                 // exception as-is. If it wasn't a Python exception, wrap it
                 // as one and continue.
                 throw (org.python.exceptions.BaseException) e.getCause();
             } catch (ClassCastException java_e) {
-                throw new org.python.exceptions.RuntimeError(e.getCause().getMessage());
+                throw new org.python.exceptions.RuntimeError(e.getCause().toString());
             }
         } catch (java.lang.InstantiationException e) {
             throw new org.python.exceptions.RuntimeError(e.getCause().toString());
