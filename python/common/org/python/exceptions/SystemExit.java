a843c49e8c916d2336611fdfc19a154d907f8499
Russell Keith-Magee <russell@keith-magee.com>
2015-11-18 11:44:27 +0800

    Added internals for SystemExit exception.

--- python/common/org/python/exceptions/SystemExit.java
+++ python/common/org/python/exceptions/SystemExit.java
@@ -1,11 +1,15 @@
 package org.python.exceptions;
 
 public class SystemExit extends org.python.exceptions.BaseException {
+    public int return_code;
+
     public SystemExit() {
-        super();
+        super("0");
+        return_code = 0;
     }
 
-    public SystemExit(String msg) {
-        super(msg);
+    public SystemExit(int val) {
+        super(java.lang.String.format("%s", val));
+        this.return_code = val;
     }
 }
