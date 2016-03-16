37dd47e6a087929936b26aa1ce7666466247a5d4
Chiang Fong Lee <myself@cflee.net>
2016-03-16 16:03:10 +0800

    Switch to using testdaemon for runAsJava()
    - Use subprocess.stdin and .stdout instead of .communicate, because the
    latter waits for the process to reply and exit.
    - Since we can't receive an EOF from testdaemon (that would require
    closing System.out, which can't be opened), and it's difficult to
    determine if the output is done, we sense for a ".\n" line that
    testdaemon specially sends to signal this.
    - Need to flush subprocess.stdin, otherwise things just hang there.

--- tests/utils.py
+++ tests/utils.py
@@ -148,21 +148,22 @@ def runAsJava(test_dir, main_code, extra_code=None, run_in_function=False, args=
     if args is None:
         args = []
 
-    classpath = os.pathsep.join([
-        os.path.join('..', '..', 'dist', 'python-java.jar'),
-        os.path.join('..', 'java'),
-        os.curdir,
-    ])
-    proc = subprocess.Popen(
-        ["java", "-classpath", classpath, "python.test.__init__"] + args,
-        stdin=subprocess.PIPE,
-        stdout=subprocess.PIPE,
-        stderr=subprocess.STDOUT,
-        cwd=test_dir,
-    )
-    out = proc.communicate()
+    global _jvm
+    # encode to turn str into bytes-like object
+    _jvm.stdin.write(("python.test.__init__\n").encode())
+    _jvm.stdin.flush()
+    out = ""
+    while True:
+        try:
+            line = _jvm.stdout.readline().decode("utf-8")
+            if line == ".\n":
+                break
+            else:
+                out += line
+        except IOError:
+            continue
 
-    return out[0].decode('utf8')
+    return out
 
 
 def compileJava(java_dir, java):
