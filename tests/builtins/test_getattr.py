3125106b7b3a035b5a6d4ca48e00316b41faac3d
Chiang Fong Lee <myself@cflee.net>
2016-03-25 13:35:25 +0800

    Add new builtins tests
    - All tests are marked as not implemented by default

---
+++ tests/builtins/test_getattr.py
@@ -0,0 +1,25 @@
+from .. utils import TranspileTestCase, BuiltinFunctionTestCase
+
+
+class GetattrTests(TranspileTestCase):
+    pass
+
+
+class BuiltinGetattrFunctionTests(BuiltinFunctionTestCase, TranspileTestCase):
+    functions = ["getattr"]
+
+    not_implemented = [
+        'test_bool',
+        'test_bytearray',
+        'test_bytes',
+        'test_class',
+        'test_complex',
+        'test_dict',
+        'test_float',
+        'test_frozenset',
+        'test_int',
+        'test_list',
+        'test_set',
+        'test_str',
+        'test_tuple',
+    ]
