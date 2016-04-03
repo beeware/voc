e8e1f2097d767ed6595a642be27c6d777ea312f7
Chiang Fong Lee <myself@cflee.net>
2016-04-03 21:52:41 +0800

    Add stubs to satisfy modified org.python.Object interface

--- python/common/org/python/Object.java
+++ python/common/org/python/Object.java
@@ -143,7 +143,7 @@ public interface Object {
     public void __ifloordiv__(org.python.Object other);
     public void __imod__(org.python.Object other);
     public void __idivmod__(org.python.Object other);
-    public void __ipow__(org.python.Object other, org.python.Object modulus);
+    public void __ipow__(org.python.Object other);
     public void __ilshift__(org.python.Object other);
     public void __irshift__(org.python.Object other);
     public void __iand__(org.python.Object other);
