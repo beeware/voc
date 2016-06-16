5c8741b8c4fe7ce447fadeeb1707144903728836
Sjoerd Job Postmus <sjoerdjob@sjec.nl>
2016-06-16 06:27:04 +0200

    Add extra testcases for `sum`.

--- tests/builtins/test_sum.py
+++ tests/builtins/test_sum.py
@@ -1,3 +1,5 @@
+from unittest import expectedFailure
+
 from .. utils import TranspileTestCase, BuiltinFunctionTestCase
 
 
@@ -18,3 +20,26 @@ class BuiltinSumFunctionTests(BuiltinFunctionTestCase, TranspileTestCase):
         'test_set',
         'test_str',
     ]
+
+    def test_sum_list(self):
+        self.assertCodeExecution("""
+            print(sum([1, 2, 3, 4, 5, 6, 7]))
+        """)
+
+    def test_sum_tuple(self):
+        self.assertCodeExecution("""
+            print(sum((1, 2, 3, 4, 5, 6, 7)))
+        """)
+
+    def test_sum_iterator(self):
+        self.assertCodeExecution("""
+            i = iter([1, 2])
+            print(sum(i))
+            print(sum(i))
+        """)
+
+    @expectedFailure  # + not defined on float/float yet.
+    def test_sum_mix_floats_and_ints(self):
+        self.assertCodeExecution("""
+            print(sum([1, 1.414, 2, 3.14159]))
+        """)
