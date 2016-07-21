d7f1a34afa8b4ba107dd1d06d2935a6f3dde10a2
Sjoerd Job Postmus <sjoerdjob@sjec.nl>
2016-07-21 18:49:29 +0200

    Replace ArrayList with Array in Map

--- python/common/org/python/types/Map.java
+++ python/common/org/python/types/Map.java
@@ -2,13 +2,13 @@ package org.python.types;
 
 public class Map extends org.python.types.Object implements org.python.Iterable {
     private org.python.Object callable;
-    private java.util.List<org.python.Iterable> iterators;
+    private org.python.Iterable[] iterators;
 
     public Map(org.python.Object callable, org.python.Object [] iterables) {
         this.callable = callable;
-        this.iterators = new java.util.ArrayList();
-        for (org.python.Object iterable: iterables) {
-            this.iterators.add(org.Python.iter(iterable));
+        this.iterators = new org.python.Iterable[iterables.length];
+        for (int i = 0; i < iterables.length; i++) {
+            this.iterators[i] = org.Python.iter(iterables[i]);
         }
     }
 
@@ -23,12 +23,9 @@ public class Map extends org.python.types.Object implements org.python.Iterable 
         __doc__ = "Implement next(self)."
     )
     public org.python.Object __next__() {
-        org.python.Object[] args = new org.python.Object[iterators.size()];
-        int i = 0;
-        for (org.python.Iterable iterator: iterators) {
-            args[i] = iterator.__next__();
-            assert(args[i] != null);
-            i++;
+        org.python.Object[] args = new org.python.Object[iterators.length];
+        for (int i = 0; i < iterators.length; i++) {
+            args[i] = iterators[i].__next__();
         }
         return ((org.python.Callable)callable).invoke(args, new java.util.HashMap());
     }
