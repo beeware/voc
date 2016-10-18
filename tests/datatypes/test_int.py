3744922d1733da1eb90e9e0db04b00c592f088dc
Italo Maia <italo.maia@gmail.com>
2016-10-18 10:34:13 -0300

    Adding bytearray multiply support to int

--- tests/datatypes/test_int.py
+++ tests/datatypes/test_int.py
@@ -65,7 +65,6 @@ class BinaryIntOperationTests(BinaryOperationTestCase, TranspileTestCase):
         'test_modulo_complex',
         'test_modulo_frozenset',
 
-        'test_multiply_bytearray',
         'test_multiply_bytes',
         'test_multiply_class',
         'test_multiply_complex',
