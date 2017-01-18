f73481cc0e5fb33e37c26c6aa9798e134aaa83f0
Chiang Fong Lee <myself@cflee.net>
2017-01-18 16:50:52 +0800

    Fix test indentation

--- tests/structures/test_for.py
+++ tests/structures/test_for.py
@@ -15,27 +15,27 @@ class ForLoopTests(TranspileTestCase):
 
         # Empty range
         self.assertCodeExecution("""
-              total = 0
-              for i in range(0, 0):
-                  total = total + i
-                  print(i, total)
-              """)
+            total = 0
+            for i in range(0, 0):
+                total = total + i
+                print(i, total)
+            """)
 
         # Stepped range
         self.assertCodeExecution("""
-              total = 0
-              for i in range(0, 10, 2):
-                  total = total + i
-                  print(i, total)
-              """)
+            total = 0
+            for i in range(0, 10, 2):
+                total = total + i
+                print(i, total)
+            """)
 
         # Reverse range
         self.assertCodeExecution("""
-              total = 0
-              for i in range(5, 0, -1):
-                  total = total + i
-                  print(i, total)
-              """)
+            total = 0
+            for i in range(5, 0, -1):
+                total = total + i
+                print(i, total)
+            """)
 
     def test_for_over_iterable(self):
         self.assertCodeExecution("""
@@ -54,57 +54,52 @@ class ForLoopTests(TranspileTestCase):
 
     @expectedFailure
     def test_for_else(self):
-        self.assertCodeExecution(
-            code="""
-                total = 0
-                for i in []:
-                    total = total + i
-                else:
-                    total = -999
-                print(total)
+        self.assertCodeExecution("""
+            total = 0
+            for i in []:
+                total = total + i
+            else:
+                total = -999
+            print(total)
             """)
 
     @expectedFailure
     def test_for_else_break(self):
-        self.assertCodeExecution(
-            code="""
-                total = 0
-                for i in []:
-                    total = total + i
-                    break
-                else:
-                    total = -999
-                print(total)
+        self.assertCodeExecution("""
+            total = 0
+            for i in []:
+                total = total + i
+                break
+            else:
+                total = -999
+            print(total)
             """)
 
     def test_break(self):
-        self.assertCodeExecution(
-            code="""
-                for i in range(1, 10):
-                    print(i, i % 5)
-                    if i % 5 == 0:
-                        break
-                    print ("after")
+        self.assertCodeExecution("""
+            for i in range(1, 10):
+                print(i, i % 5)
+                if i % 5 == 0:
+                    break
+                print("after")
             """)
 
     def test_continue(self):
-        self.assertCodeExecution(
-            code="""
-                for i in range(1, 10):
-                    print(i, i % 5)
-                    if i % 5 == 0:
-                        continue
-                    print ("after")
+        self.assertCodeExecution("""
+            for i in range(1, 10):
+                print(i, i % 5)
+                if i % 5 == 0:
+                    continue
+                print("after")
             """)
 
     def test_nested(self):
-        self.assertCodeExecution(
-            code="""
-                i = 1
-                j = 10
-                for i in range(1, 10):
-                    for k in range(0, i):
-                        print(i, j)
+        self.assertCodeExecution("""
+            i = 1
+            j = 10
+            for i in range(1, 10):
+                for k in range(0, i):
+                    print(i, j)
             """)
 
     def test_multiple_values(self):
--- tests/structures/test_while.py
+++ tests/structures/test_while.py
@@ -15,109 +15,100 @@ class WhileLoopTests(TranspileTestCase):
             """)
 
     def test_break(self):
-        self.assertCodeExecution(
-            code="""
-                i = 0
-                while i < 10:
-                    i = i + 1
-                    print(i, i % 5)
-                    if i % 5 == 0:
-                        break
-                    print("after")
+        self.assertCodeExecution("""
+            i = 0
+            while i < 10:
+                i = i + 1
+                print(i, i % 5)
+                if i % 5 == 0:
+                    break
+                print("after")
             """)
 
     def test_continue(self):
-        self.assertCodeExecution(
-            code="""
-                i = 0
-                while i < 10:
-                    i = i + 1
-                    print(i, i % 5)
-                    if i % 5 == 0:
-                        continue
-                    print("after")
+        self.assertCodeExecution("""
+            i = 0
+            while i < 10:
+                i = i + 1
+                print(i, i % 5)
+                if i % 5 == 0:
+                    continue
+                print("after")
             """)
 
     def test_nested(self):
-        self.assertCodeExecution(
-            code="""
-                i = 1
-                j = 10
-                while i < j:
-                    k = 0
-                    while k < i:
-                        print(i, j)
-                        k = k + 1
-                    print("While done")
-                    i = i + 1
+        self.assertCodeExecution("""
+            i = 1
+            j = 10
+            while i < j:
+                k = 0
+                while k < i:
+                    print(i, j)
+                    k = k + 1
+                print("While done")
+                i = i + 1
             """)
 
     def test_while_forever(self):
-        self.assertCodeExecution(
-            code="""
-                i = 0
+        self.assertCodeExecution("""
+            i = 0
+            while 1:
+                print("Loop", i)
+                i = i + 1
+                if i == 10:
+                    break
+            """)
+
+    def test_while_forever_with_if_not(self):
+        self.assertCodeExecution("""
+            i = 0
+            while 1:
+                print("Loop", i)
+                i = i + 1
+                if not i < 10:
+                    break
+            """)
+
+    def test_while_not_forever(self):
+        self.assertCodeExecution("""
+            while not 0:
+                print("Loop")
+                break
+            """)
+
+    @expectedFailure
+    def test_while_else(self):
+        self.assertCodeExecution("""
+            i = 1
+            j = 4
+            while i < j:
+                print(i)
+                i = i + 1
+            else:
+                print("Else")
+            """)
+
+    def test_while_else_break(self):
+        self.assertCodeExecution("""
+            i = 1
+            j = 4
+            while i < j:
+                print(i)
+                i = i + 1
+                break
+            else:
+                print("Else")
+            """)
+
+    def test_while_forever_inside_try(self):
+        self.assertCodeExecution("""
+            i = 0
+            try:
                 while 1:
                     print("Loop", i)
                     i = i + 1
                     if i == 10:
                         break
-            """)
-
-    def test_while_forever_with_if_not(self):
-        self.assertCodeExecution(
-            code="""
-                i = 0
-                while 1:
-                    print("Loop", i)
-                    i = i + 1
-                    if not i < 10:
-                        break
-            """)
-
-    def test_while_not_forever(self):
-        self.assertCodeExecution(
-            code="""
-                while not 0:
-                    print("Loop")
-                    break
-            """)
-
-    @expectedFailure
-    def test_while_else(self):
-        self.assertCodeExecution(
-            code="""
-                i = 1
-                j = 4
-                while i < j:
-                    print(i)
-                    i = i + 1
-                else:
-                    print("Else")
-            """)
-
-    def test_while_else_break(self):
-        self.assertCodeExecution(
-            code="""
-                i = 1
-                j = 4
-                while i < j:
-                    print(i)
-                    i = i + 1
-                    break
-                else:
-                    print("Else")
-            """)
-
-    def test_while_forever_inside_try(self):
-        self.assertCodeExecution(
-            code="""
-                i = 0
-                try:
-                    while 1:
-                        print("Loop", i)
-                        i = i + 1
-                        if i == 10:
-                            break
-                finally:
-                    print("Done")
+            finally:
+                print("Done")
             """)
