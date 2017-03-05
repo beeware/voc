40fe9ab7ed665a8dfb8fdb5c90666dd40b4c5835
deepankar <deepankar11093@gmail.com>
2017-03-05 23:08:28 +0530

    adds iter implementation to dict

--- python/common/org/python/types/Dict.java
+++ python/common/org/python/types/Dict.java
@@ -308,7 +308,7 @@ public class Dict extends org.python.types.Object {
     )
     public org.python.Iterable __iter__() {
         // FIXME: Once this is implemented, update org.Python.addToKwargs()
-        throw new org.python.exceptions.NotImplementedError("dict.__iter__() has not been implemented.");
+        return new org.python.types.Iterator(new org.python.types.List(new java.util.ArrayList<org.python.Object>(this.value.keySet())));
     }
 
     @org.python.Method(
