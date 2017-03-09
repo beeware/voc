72487bf7144a479fea5baf30bbc4569a86a01f94
deepankar <deepankar11093@gmail.com>
2017-03-09 22:48:01 +0530

    fix issue#369 list comprehensions causes runtime crash

--- tests/structures/test_list_comprehension.py
+++ tests/structures/test_list_comprehension.py
@@ -8,6 +8,11 @@ class ListComprehensionTests(TranspileTestCase):
             print([v**2 for v in x])
             """)
 
+        self.assertCodeExecution("""
+            x = [1, 2, 3, 4, 5]
+            print([v for v in x])
+            """)
+
     def test_method(self):
         self.assertCodeExecution("""
             x = [1, 2, 3, 4, 5]
