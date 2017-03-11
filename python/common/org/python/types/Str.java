57dde2a38c2012f30f90ed40bad636d88c4b21c6
gilmouta <gilmouta@gmail.com>
2017-03-11 19:23:11 +0000

    Further code formatting fixes

--- python/common/org/python/types/Str.java
+++ python/common/org/python/types/Str.java
@@ -902,16 +902,16 @@ public class Str extends org.python.types.Object {
     }
 
     @org.python.Method(
-        __doc__ = "S.partition(sep) -> tuple",
-        args = {"sep"}
+            __doc__ = "S.partition(sep) -> tuple",
+            args = {"sep"}
     )
     public org.python.Object partition(org.python.Object sep) {
         if (sep instanceof org.python.types.Str) {
-            String sepStr = ((org.python.types.Str)sep).value;
-            if (sepStr.equals("")){
+            String sepStr = ((org.python.types.Str) sep).value;
+            if (sepStr.equals("")) {
                 throw new org.python.exceptions.ValueError("empty separator");
             }
-            String[] split = this.value.split(sepStr,2);
+            String[] split = this.value.split(sepStr, 2);
             java.util.List<org.python.Object> tuple = new java.util.ArrayList<org.python.Object>();
             tuple.add(new org.python.types.Str(split[0]));
             tuple.add(sep);
