2f5ec4d9a168008d8506b4be38eeff148c033ca7
Russell Keith-Magee <russell@keith-magee.com>
2017-03-30 10:03:02 +0800

    Added implementation of max and min.

--- tests/builtins/test_max.py
+++ tests/builtins/test_max.py
@@ -1,29 +1,80 @@
+from unittest import expectedFailure
+
 from .. utils import TranspileTestCase, BuiltinFunctionTestCase
 
 
 class MaxTests(TranspileTestCase):
-    pass
+    def test_args(self):
+        self.assertCodeExecution("""
+            try:
+                max()
+            except TypeError:
+                print("Threw an error as expected")
+
+            # Single integer
+            try:
+                max(4)
+            except TypeError:
+                print("Threw an error as expected")
+
+            # Multiple integers
+            print(max(1, 5, 2, 4))
+
+            # String (an iterable)
+            print(max("Hello World"))
+
+            # Multiple strings
+            print(max("Hello World", "Goodbye World"))
+            """)
+
+    def test_iterable(self):
+        self.assertCodeExecution("""
+            # Empty iterable
+            try:
+                max([])
+            except ValueError:
+                print("Threw an error as expected")
+
+            # Single iterable argument
+            print(max([1, 5, 2, 4]))
+
+            # Multiple iterables
+            print(max([1, 6], [1, 5, 2, 4]))
+            """)
+
+    def test_default(self):
+        self.assertCodeExecution("""
+            # Empty iterable
+            print(max([], default=42))
+
+            # Single iterable argument
+            print(max([1, 5, 2, 4], default=42))
+
+            # Multiple iterables
+            try:
+                print(max([1, 6], [1, 5, 2, 4], default=42))
+            except TypeError:
+                print("Threw an error as expected")
+            """)
+
+    @expectedFailure
+    def test_key(self):
+        self.assertCodeExecution("""
+            # key applied over args
+            print(max(51, 42, 33, 24, key=lambda v: v % 10))
+
+            # key applied over iterable
+            print(max([51, 42, 33, 24], key=lambda v: v % 10))
+            """)
 
 
 class BuiltinMaxFunctionTests(BuiltinFunctionTestCase, TranspileTestCase):
     functions = ["max"]
 
     not_implemented = [
-        'test_bool',
         'test_bytearray',
         'test_bytes',
-        'test_class',
-        'test_complex',
-        'test_dict',
-        'test_float',
         'test_frozenset',
-        'test_int',
-        'test_list',
-        'test_None',
-        'test_NotImplemented',
-        'test_range',
         'test_set',
-        'test_slice',
-        'test_str',
         'test_tuple',
     ]
