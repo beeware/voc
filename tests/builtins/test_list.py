58f6f90b1f06acc3caaddb0d522a1934f59d2dfa
Russell Keith-Magee <russell@keith-magee.com>
2017-06-13 16:36:58 +0800

    Added substitutions for list ordering.

--- tests/builtins/test_list.py
+++ tests/builtins/test_list.py
@@ -9,15 +9,23 @@ class BuiltinListFunctionTests(BuiltinFunctionTestCase, TranspileTestCase):
     functions = ["list"]
 
     substitutions = dict(SAMPLE_SUBSTITUTIONS)
-    substitutions["[1, 2.3456, 'another']"] = [
-        "[1, 'another', 2.3456]",
-        "[2.3456, 1, 'another']",
-        "[2.3456, 'another', 1]",
-        "['another', 1, 2.3456]",
-        "['another', 2.3456, 1]",
-    ]
-
+    substitutions.update({
+        "[1, 2.3456, 'another']": [
+            "[1, 'another', 2.3456]",
+            "[2.3456, 1, 'another']",
+            "[2.3456, 'another', 1]",
+            "['another', 1, 2.3456]",
+            "['another', 2.3456, 1]",
+        ],
+        "['a', 'c', 'd']": [
+            "['a', 'd', 'c']",
+            "['c', 'a', 'd']",
+            "['c', 'd', 'a']",
+            "['d', 'a', 'c']",
+            "['d', 'c', 'a']",
+        ]
+    })
+    print(substitutions)
     not_implemented = [
         'test_bytearray',
-        'test_dict',
     ]
