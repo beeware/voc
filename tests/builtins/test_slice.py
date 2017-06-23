f6a24802f08afeb823695cc8bae9fdefaffba6e4
Tzu-ping Chung <uranusjr@gmail.com>
2017-06-23 10:36:53 +0800

    Add simple tests to make sure slice() works

--- tests/builtins/test_slice.py
+++ tests/builtins/test_slice.py
@@ -2,7 +2,23 @@ from .. utils import TranspileTestCase, BuiltinFunctionTestCase
 
 
 class SliceTests(TranspileTestCase):
-    pass
+    def test_slice_repr_stop(self):
+        self.assertCodeExecution("""
+            print(slice(0))
+            print(slice('foo'))
+            """)
+
+    def test_slice_repr_start_stop(self):
+        self.assertCodeExecution("""
+            print(slice(0, 100))
+            print(slice('foo', Exception))
+            """)
+
+    def test_slice_repr_start_stop_step(self):
+        self.assertCodeExecution("""
+            print(slice(0, 100, 2))
+            print(slice('foo', Exception, object()))
+            """)
 
 
 class BuiltinSliceFunctionTests(BuiltinFunctionTestCase, TranspileTestCase):
