3748c312e3c88ff3c39bdde913087dca61823dad
Gonzalo Pena-Castellanos <goanpeca@gmail.com>
2017-08-11 19:40:43 -0400

    Fix output dir issues

--- compile_stdlib.py
+++ compile_stdlib.py
@@ -7,7 +7,8 @@ from subprocess import Popen, PIPE
 import multiprocessing
 import os
 
-HERE = os.path.dirname(os.path.realpath(__file__))
+
+REPO_ROOT = os.path.dirname(os.path.realpath(__file__))
 MODULES_NOT_COMPILING = [
     'ouroboros/__init__.py',
     'ouroboros/_collections_abc.py',
@@ -20,7 +21,6 @@ MODULES_NOT_COMPILING = [
     'ouroboros/abc.py',
     'ouroboros/aifc.py',
     'ouroboros/argparse.py',
-    'ouroboros/asyncio/__init__.py',
     'ouroboros/asyncio/base_events.py',
     'ouroboros/asyncio/base_subprocess.py',
     'ouroboros/asyncio/coroutines.py',
@@ -43,29 +43,21 @@ MODULES_NOT_COMPILING = [
     'ouroboros/codecs.py',
     'ouroboros/collections/__init__.py',
     'ouroboros/compileall.py',
-    'ouroboros/concurrent/__init__.py',
-    'ouroboros/concurrent/futures/__init__.py',
     'ouroboros/concurrent/futures/_base.py',
     'ouroboros/concurrent/futures/process.py',
     'ouroboros/contextlib.py',
-    'ouroboros/ctypes/__init__.py',
     'ouroboros/ctypes/_endian.py',
-    'ouroboros/ctypes/macholib/__init__.py',
-    'ouroboros/curses/__init__.py',
     'ouroboros/curses/textpad.py',
     'ouroboros/datetime.py',
     'ouroboros/dbm/__init__.py',
     'ouroboros/decimal.py',
     'ouroboros/difflib.py',
-    'ouroboros/distutils/__init__.py',
     'ouroboros/distutils/ccompiler.py',
-    'ouroboros/distutils/command/__init__.py',
     'ouroboros/distutils/command/build_py.py',
     'ouroboros/distutils/command/register.py',
     'ouroboros/distutils/dist.py',
     'ouroboros/distutils/fancy_getopt.py',
     'ouroboros/doctest.py',
-    'ouroboros/email/__init__.py',
     'ouroboros/email/_encoded_words.py',
     'ouroboros/email/_header_value_parser.py',
     'ouroboros/email/_policybase.py',
@@ -75,11 +67,8 @@ MODULES_NOT_COMPILING = [
     'ouroboros/email/headerregistry.py',
     'ouroboros/email/iterators.py',
     'ouroboros/email/message.py',
-    'ouroboros/email/mime/__init__.py',
     'ouroboros/email/mime/nonmultipart.py',
-    'ouroboros/encodings/__init__.py',
     'ouroboros/encodings/uu_codec.py',
-    'ouroboros/ensurepip/__init__.py',
     'ouroboros/enum.py',
     'ouroboros/filecmp.py',
     'ouroboros/ftplib.py',
@@ -88,8 +77,6 @@ MODULES_NOT_COMPILING = [
     'ouroboros/gettext.py',
     'ouroboros/glob.py',
     'ouroboros/heapq.py',
-    'ouroboros/html/__init__.py',
-    'ouroboros/http/__init__.py',
     'ouroboros/http/cookiejar.py',
     'ouroboros/http/server.py',
     'ouroboros/idlelib/EditorWindow.py',
@@ -102,8 +89,6 @@ MODULES_NOT_COMPILING = [
     'ouroboros/idlelib/ScriptBinding.py',
     'ouroboros/idlelib/SearchEngine.py',
     'ouroboros/idlelib/WidgetRedirector.py',
-    'ouroboros/idlelib/__init__.py',
-    'ouroboros/idlelib/idle_test/__init__.py',
     'ouroboros/idlelib/idle_test/htest.py',
     'ouroboros/idlelib/idle_test/mock_idle.py',
     'ouroboros/idlelib/macosxSupport.py',
@@ -111,25 +96,19 @@ MODULES_NOT_COMPILING = [
     'ouroboros/idlelib/run.py',
     'ouroboros/idlelib/tabbedpages.py',
     'ouroboros/imaplib.py',
-    'ouroboros/importlib/__init__.py',
     'ouroboros/importlib/abc.py',
     'ouroboros/importlib/util.py',
     'ouroboros/inspect.py',
     'ouroboros/io.py',
     'ouroboros/ipaddress.py',
     'ouroboros/itertools.py',
-    'ouroboros/json/__init__.py',
     'ouroboros/json/encoder.py',
-    'ouroboros/lib2to3/__init__.py',
     'ouroboros/lib2to3/btm_utils.py',
-    'ouroboros/lib2to3/fixes/__init__.py',
     'ouroboros/lib2to3/main.py',
     'ouroboros/lib2to3/patcomp.py',
-    'ouroboros/lib2to3/pgen2/__init__.py',
     'ouroboros/lib2to3/pgen2/driver.py',
     'ouroboros/lib2to3/pytree.py',
     'ouroboros/lib2to3/refactor.py',
-    'ouroboros/logging/__init__.py',
     'ouroboros/logging/config.py',
     'ouroboros/logging/handlers.py',
     'ouroboros/lzma.py',
@@ -137,11 +116,8 @@ MODULES_NOT_COMPILING = [
     'ouroboros/mailbox.py',
     'ouroboros/mimetypes.py',
     'ouroboros/modulefinder.py',
-    'ouroboros/msilib/__init__.py',
     'ouroboros/msilib/schema.py',
-    'ouroboros/multiprocessing/__init__.py',
     'ouroboros/multiprocessing/connection.py',
-    'ouroboros/multiprocessing/dummy/__init__.py',
     'ouroboros/multiprocessing/pool.py',
     'ouroboros/netrc.py',
     'ouroboros/numbers.py',
@@ -160,7 +136,6 @@ MODULES_NOT_COMPILING = [
     'ouroboros/pstats.py',
     'ouroboros/pty.py',
     'ouroboros/pydoc.py',
-    'ouroboros/pydoc_data/__init__.py',
     'ouroboros/quopri.py',
     'ouroboros/selectors.py',
     'ouroboros/shutil.py',
@@ -168,7 +143,6 @@ MODULES_NOT_COMPILING = [
     'ouroboros/smtplib.py',
     'ouroboros/sndhdr.py',
     'ouroboros/socketserver.py',
-    'ouroboros/sqlite3/__init__.py',
     'ouroboros/sre_compile.py',
     'ouroboros/string.py',
     'ouroboros/tabnanny.py',
@@ -181,65 +155,70 @@ MODULES_NOT_COMPILING = [
     'ouroboros/trace.py',
     'ouroboros/traceback.py',
     'ouroboros/turtle.py',
-    'ouroboros/turtledemo/__init__.py',
     'ouroboros/turtledemo/__main__.py',
-    'ouroboros/unittest/__init__.py',
     'ouroboros/unittest/case.py',
     'ouroboros/unittest/loader.py',
     'ouroboros/unittest/mock.py',
-    'ouroboros/urllib/__init__.py',
     'ouroboros/urllib/parse.py',
     'ouroboros/urllib/request.py',
     'ouroboros/urllib/robotparser.py',
     'ouroboros/uu.py',
     'ouroboros/uuid.py',
-    'ouroboros/venv/__init__.py',
     'ouroboros/weakref.py',
     'ouroboros/webbrowser.py',
-    'ouroboros/wsgiref/__init__.py',
     'ouroboros/wsgiref/handlers.py',
     'ouroboros/xdrlib.py',
-    'ouroboros/xml/__init__.py',
-    'ouroboros/xml/dom/__init__.py',
     'ouroboros/xml/dom/minicompat.py',
     'ouroboros/xml/dom/minidom.py',
     'ouroboros/xml/dom/xmlbuilder.py',
     'ouroboros/xml/etree/ElementPath.py',
     'ouroboros/xml/etree/ElementTree.py',
-    'ouroboros/xml/etree/__init__.py',
-    'ouroboros/xml/parsers/__init__.py',
-    'ouroboros/xml/sax/__init__.py',
     'ouroboros/xml/sax/expatreader.py',
-    'ouroboros/xmlrpc/__init__.py',
     'ouroboros/xmlrpc/client.py',
     'ouroboros/xmlrpc/server.py',
     'ouroboros/zipfile.py',
 ]
 
 # This modules are not checked as Java native implementations are provided
-MODULE_OVERRIDES = [
+MODULE_MANUAL_OVERRIDES = [
+    # 'ouroboros/somefiletoignore1.py',
+    # 'ouroboros/somefiletoignore2.py',
 ]
 
 
-def get_module_overrides():
-    pass
-    #os.python/common/python
-
-def ouroboros_folder():
+def ouroboros_repo_folder():
     """Return the folder where the ouroboros repo was cloned."""
-    path = os.path.realpath(os.path.join(HERE))
-    path = os.path.dirname(path)
-    path = os.path.join(path, 'ouroborus-repo') + os.sep
-
+    path = os.path.join(REPO_ROOT, 'ouroboros-repo')
     if not os.path.isdir(path):
-        os.makedirs(path)        
-
+        os.makedirs(path)
     return path
 
 
-def compiled_files_folder():
+def ouroboros_module_folder():
+    """Return the folder where the ouroboros module lives (inside the repo)."""
+    return os.path.join(ouroboros_repo_folder(), 'ouroboros')
+
+
+def ouroboros_compiled_folder(target='java'):
+    """Location of the `voc` compiled `*.java` files."""
+    return os.path.join(REPO_ROOT, 'build', target)
+
+
+def module_overrides():
     """"""
-    return os.path.join(ouroboros_folder(), 'ouroboros-compiled')
+    folder_overrides = os.path.join(REPO_ROOT, 'python', 'common', 'python')
+    paths = []
+    for folder, subfolders, files in os.walk(folder_overrides):
+        for fname in files:
+            if fname.endswith('.java'):
+                module_path = os.path.join('ouroboros',
+                                           fname.replace('.java', '.py'))
+                module_init_path = os.path.join('ouroboros',
+                                           fname.replace('.java', ''),
+                                           '__init__.py')
+                paths += [module_path, module_init_path]
+    paths += MODULE_MANUAL_OVERRIDES
+    return list(sorted(paths))
 
 
 def clone_update_repo():
@@ -248,7 +227,7 @@ def clone_update_repo():
     print('=======================')
     url = 'https://github.com/pybee/ouroboros.git'
 
-    repo_folder = ouroboros_folder()
+    repo_folder = ouroboros_repo_folder()
     if os.path.isdir(os.path.join(repo_folder, '.git')):
         print('Pulling from master on repo: {}\n'.format(url))
         args = ['git', 'pull', 'origin', 'master']
@@ -272,17 +251,22 @@ def list_files():
     We currently exclude paths including '/test/' or '/tests/' and files
     with extensions different from `.py`.
     """
+    overrides = module_overrides()
     paths = []
-    repo_folder = ouroboros_folder()
-    for folder, subfolders, files in os.walk(repo_folder ):
+    repo_folder = ouroboros_repo_folder()
+    for folder, subfolders, files in os.walk(ouroboros_module_folder()):
         for fname in files:
             if not fname.startswith('test_') and fname.endswith('.py'):
                 path = os.path.join(folder, fname)
-                norm_path = 'ouroboros' + path.replace(repo_folder, '')
-                if ('/test/' not in path and '/tests/' not in path and
-                        norm_path not in MODULE_OVERRIDES):
+                normpath = path.replace(repo_folder , '')
+                if (os.sep + 'test' + os.sep not in path and
+                        os.sep + 'tests' + os.sep not in path and
+                        normpath not in overrides):
                     paths.append(path)
-    return paths
+
+    normal_paths = [p for p in paths if not p.endswith('__init__.py')]
+    init_paths = [p for p in paths if p.endswith('__init__.py')]
+    return normal_paths + init_paths
 
 
 def compile_with_voc(args):
@@ -291,17 +275,20 @@ def compile_with_voc(args):
 
     Save results in the queues passed by pool.map.
     """
-    path, failed_queue, xpassed_queue = args
-    folder = ouroboros_folder()
-    norm_path = path.replace(folder, '')
-    args = ['voc', '-v', path, '-o', compiled_files_folder()]
+    namespace = 'python'
+    path, failed_queue, xpassed_queue, target = args
+    folder = os.path.dirname(ouroboros_repo_folder())
+    norm_path = path.replace(folder + os.sep, '')
+    args = ['voc', '-v', path, '-o', ouroboros_compiled_folder(target=target),
+            '--prefix', ouroboros_module_folder()]
 
     p = Popen(args, stdout=PIPE, stderr=PIPE, cwd=os.path.dirname(path))
     stdout, stderr = p.communicate()
     stdout = stdout.decode()
     stderr = stderr.decode()
 
-    check = [i for i in MODULES_NOT_COMPILING if path.endswith(i)]
+    check = [i for i in MODULES_NOT_COMPILING if
+             path.replace('\\', '/').endswith(i)]
 
     if stderr and not check:
         # Unexpected failure
@@ -312,20 +299,25 @@ def compile_with_voc(args):
         xpassed_queue.put((norm_path, stdout, stderr))
         print('FAILED: {}'.format(norm_path))
     elif not stderr and not check:
-        print('PASSED: {}'.format(norm_path))
+        module_path = norm_path.replace('ouroboros-repo' + os.sep + 'ouroboros', '')
+        norm_compiled_path = (os.path.join(ouroboros_compiled_folder(target),
+                                           namespace) + module_path)
+        norm_compiled_path = norm_compiled_path.replace(REPO_ROOT + os.sep, '')
+        norm_compiled_path = norm_compiled_path.replace('.py', '.class')
+        print('PASSED: {0} -> {1}'.format(norm_path, norm_compiled_path))
     elif stderr and not not check:
         print('xfail:  {}'.format(norm_path))
- 
+
     return stdout, stderr
 
 
-def run_process(paths):
+def run_process(paths, target='java'):
     """Run main compilation process on all paths found."""
     pool = multiprocessing.Pool(multiprocessing.cpu_count())
     m = multiprocessing.Manager()
     xpassed_queue = m.Queue()
     failed_queue  = m.Queue()
-    args = [(path, failed_queue, xpassed_queue) for path in paths]
+    args = [(path, failed_queue, xpassed_queue, target) for path in paths]
 
     print('\nSTART VOC COMPILE TESTS')
     print('=======================')
@@ -360,12 +352,13 @@ def run_process(paths):
     return failed_list, xpassed_list
 
 
-if __name__ == '__main__':
+def main(target='java'):
+    """"""
     start = datetime.now()
     clone_update_repo()
     paths = list_files()
 
-    print('\nFound: {} files\n'.format(len(paths)))
+    print('\nFound: {} python files\n'.format(len(paths)))
 
     failed_list , xpassed_list = run_process(paths)
     n_err = len(failed_list) + len(xpassed_list)
@@ -381,3 +374,7 @@ if __name__ == '__main__':
         plural = 's' if n_err != 1 else ''
         raise SystemExit('\n\n{0} failure{1} encountered!!!\n\n'
                          ''.format(n_err, plural))
+
+
+if __name__ == '__main__':
+    main()
