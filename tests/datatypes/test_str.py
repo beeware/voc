245e1cb9c79cc00d976b299da2d41bb65bb3c0e6
janmax <mail-github@jmx.io>
2017-08-24 18:12:02 +0200

    Removed tests that were commented out.

--- tests/datatypes/test_str.py
+++ tests/datatypes/test_str.py
@@ -743,21 +743,7 @@ class BinaryStrOperationTests(BinaryOperationTestCase, TranspileTestCase):
     data_type = 'str'
 
     not_implemented = [
-        # 'test_modulo_bool',
-        # 'test_modulo_bytes',
-        # 'test_modulo_bytearray',
         'test_modulo_class',
-        # 'test_modulo_complex',
-        # 'test_modulo_dict',
-        # 'test_modulo_float',
-        # 'test_modulo_frozenset',
-        # 'test_modulo_slice',
-        # 'test_modulo_list',
-        # 'test_modulo_None',
-        # 'test_modulo_NotImplemented',
-        # 'test_modulo_set',
-        # 'test_modulo_str',
-        # 'test_modulo_tuple',
 
         'test_subscr_bool',
         'test_subscr_slice',
@@ -768,19 +754,5 @@ class InplaceStrOperationTests(InplaceOperationTestCase, TranspileTestCase):
     data_type = 'str'
 
     not_implemented = [
-        # 'test_modulo_bool',
-        # 'test_modulo_bytes',
-        # 'test_modulo_bytearray',
         'test_modulo_class',
-        # 'test_modulo_complex',
-        # 'test_modulo_dict',
-        # 'test_modulo_float',
-        # 'test_modulo_frozenset',
-        # 'test_modulo_slice',
-        # 'test_modulo_list',
-        # 'test_modulo_None',
-        # 'test_modulo_NotImplemented',
-        # 'test_modulo_set',
-        # 'test_modulo_str',
-        # 'test_modulo_tuple',
     ]
