b3595ad0d4c906eef93bb9edf6a431aa6548c917
Day <dayannefernandesc@gmail.com>
2017-10-10 05:05:02 -0300

    add isspace for bytes and byte array

--- python/common/org/python/types/Bytes.java
+++ python/common/org/python/types/Bytes.java
@@ -969,11 +969,23 @@ public class Bytes extends org.python.types.Object {
         return new Bool(_islower(this.value));
     }
 
+    public static boolean _isspace(byte[] input) {
+        if (input.length == 0) {
+            return false;
+        }
+        for (byte ch : input) {
+            if (ch != ' ') {
+                return false;
+            }
+        }
+        return true;
+    }
+
     @org.python.Method(
             __doc__ = "B.isspace() -> bool\n\nReturn True if all characters in B are whitespace\nand there is at least one character in B, False otherwise."
     )
-    public org.python.Object isspace(java.util.List<org.python.Object> args, java.util.Map<java.lang.String, org.python.Object> kwargs, java.util.List<org.python.Object> default_args, java.util.Map<java.lang.String, org.python.Object> default_kwargs) {
-        throw new org.python.exceptions.NotImplementedError("bytes.isspace has not been implemented.");
+    public org.python.Object isspace() {
+        return new Bool(_isspace(this.value));
     }
 
     @org.python.Method(
