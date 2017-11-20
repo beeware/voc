0c4f04d6cb1309fcc7b082a3b099f1aa3b775ae2
Jeff Smith <whydoubt@gmail.com>
2017-11-20 00:48:35 -0600

    bytearray: implement missing parts of bytearray()

--- tests/builtins/test_bytearray.py
+++ tests/builtins/test_bytearray.py
@@ -7,20 +7,3 @@ class BytearrayTests(TranspileTestCase):
 
 class BuiltinBytearrayFunctionTests(BuiltinFunctionTestCase, TranspileTestCase):
     functions = ["bytearray"]
-
-    not_implemented = [
-        'test_bool',
-        'test_class',
-        'test_complex',
-        'test_dict',
-        'test_float',
-        'test_frozenset',
-        'test_int',
-        'test_list',
-        'test_None',
-        'test_NotImplemented',
-        'test_set',
-        'test_slice',
-        'test_str',
-        'test_tuple',
-    ]
