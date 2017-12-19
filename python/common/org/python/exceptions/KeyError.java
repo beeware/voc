e4933d278993b3741ffc0a3375017fc9528694df
Anirudha Bose <ani07nov@gmail.com>
2017-12-19 21:01:31 +0100

    Fix KeyError repr for Java caller code

--- python/common/org/python/exceptions/KeyError.java
+++ python/common/org/python/exceptions/KeyError.java
@@ -16,6 +16,6 @@ public class KeyError extends org.python.exceptions.LookupError {
     }
 
     public KeyError(org.python.Object key) {
-        super(key.__repr__().toString());
+        super(new org.python.Object[] {key}, null);
     }
 }
