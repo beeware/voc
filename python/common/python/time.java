4110705abc32c9ac575a7b2e5f504d2ead9a5bcb
Prithvi Singh <prithvi.chandla@gmail.com>
2018-02-26 19:54:06 +0530

    lint fix

--- python/common/python/time.java
+++ python/common/python/time.java
@@ -100,15 +100,15 @@ public class time extends org.python.types.Module {
                       "errors defaults to 'strict'."
     )
     public static org.python.Object ctime() {
-            long currentDateTime = System.currentTimeMillis();
-            java.util.Date currentDate = new java.util.Date(currentDateTime);
-            java.text.SimpleDateFormat ft = new java.text.SimpleDateFormat ("E MMM dd HH:mm:ss yyyy");
-            return new org.python.types.Str(ft.format(currentDate));
+        long currentDateTime = System.currentTimeMillis();
+        java.util.Date currentDate = new java.util.Date(currentDateTime);
+        java.text.SimpleDateFormat ft = new java.text.SimpleDateFormat ("E MMM dd HH:mm:ss yyyy");
+        return new org.python.types.Str(ft.format(currentDate));
     }
     public static org.python.Object ctime(org.python.Object seconds) {
-            java.util.Date currentDate = new java.util.Date(((org.python.types.Int) seconds.__int__()).value * 1000);
-            java.text.SimpleDateFormat ft = new java.text.SimpleDateFormat ("E MMM dd HH:mm:ss yyyy");
-            return new org.python.types.Str(ft.format(currentDate));
+        java.util.Date currentDate = new java.util.Date(((org.python.types.Int) seconds.__int__()).value * 1000);
+        java.text.SimpleDateFormat ft = new java.text.SimpleDateFormat ("E MMM dd HH:mm:ss yyyy");
+        return new org.python.types.Str(ft.format(currentDate));
     }
 
     public static org.python.types.Int daylight;
