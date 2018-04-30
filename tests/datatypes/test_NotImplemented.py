1631afb3851e43e2cd1bcca3e404ebf8967fe626
Patience Shyu <patienceshyu@gmail.com>
2018-04-30 11:25:48 +0200

    Fix error messages for NotImplemented operations
    - Add special error messages for Bytes/ByteArray/Str/Tuple/List types
    - Remove comparison test cases that aren't failures

--- tests/datatypes/test_NotImplemented.py
+++ tests/datatypes/test_NotImplemented.py
@@ -16,55 +16,6 @@ class UnaryNotImplementedOperationTests(UnaryOperationTestCase, TranspileTestCas
 class BinaryNotImplementedOperationTests(BinaryOperationTestCase, TranspileTestCase):
     data_type = 'NotImplemented'
 
-    not_implemented = [
-        'test_multiply_bytes',
-        'test_multiply_bytearray',
-    ]
-
 
 class InplaceNotImplementedOperationTests(InplaceOperationTestCase, TranspileTestCase):
     data_type = 'NotImplemented'
-
-    not_implemented = [
-        'test_eq_bytearray',
-        'test_eq_class',
-        'test_eq_complex',
-        'test_eq_frozenset',
-
-        'test_ge_bytearray',
-        'test_ge_class',
-        'test_ge_complex',
-        'test_ge_frozenset',
-
-        'test_gt_bytearray',
-        'test_gt_class',
-        'test_gt_complex',
-        'test_gt_frozenset',
-
-        'test_le_bytearray',
-        'test_le_class',
-        'test_le_complex',
-        'test_le_frozenset',
-
-        'test_lt_bytearray',
-        'test_lt_class',
-        'test_lt_complex',
-        'test_lt_frozenset',
-
-        'test_multiply_bytes',
-        'test_multiply_bytearray',
-        'test_multiply_list',
-        'test_multiply_str',
-        'test_multiply_tuple',
-
-        'test_ne_bytearray',
-        'test_ne_class',
-        'test_ne_complex',
-        'test_ne_frozenset',
-
-        'test_subscr_bytearray',
-        'test_subscr_bytes',
-        'test_subscr_class',
-        'test_subscr_complex',
-        'test_subscr_frozenset',
-    ]
