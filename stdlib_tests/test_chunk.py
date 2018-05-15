0d4c405a0e4509ade2f157cffb2bd8cd9e9f835b
Boris Sadkhin <boris@bortech.us>
2018-05-15 17:17:18 -0400

    Add test for chunk

--- stdlib_tests/test_chunk.py
+++ stdlib_tests/test_chunk.py
@@ -1,13 +1,15 @@
 import chunk
-import io
 
-fakeFile= io.BytesIO(b"some initial binary data: \x00\x01")
-testChunk = chunk.Chunk(fakeFile)
+
+class fake_file():
+    def read(self, n=0):
+        return b"some initial binary data:"
+
+
+testChunk = chunk.Chunk(fake_file())
 
 print("Name = ", testChunk.getname())
 print("Size = ", testChunk.getsize())
 print("IsAtty = ", testChunk.isatty())
 print("Read0 = ", testChunk.read(5))
 print("Tell = ", testChunk.tell())
-testChunk.skip()
-
