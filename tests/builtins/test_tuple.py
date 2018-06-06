54408807d9b398797449127c59c7d891c26b1c88
AmirYalamov <amir.yalamov@gmail.com>
2018-06-05 22:55:11 -0400

    properly fix indentation

--- tests/builtins/test_tuple.py
+++ tests/builtins/test_tuple.py
@@ -15,23 +15,22 @@ class BuiltinTupleFunctionTests(BuiltinFunctionTestCase, TranspileTestCase):
         ]
     }
 
-
-not_implemented = [
-    'test_bool',
-    'test_bytearray',
-    'test_bytes',
-    'test_class',
-    'test_complex',
-    'test_dict',
-    'test_float',
-    'test_frozenset',
-    'test_int',
-    'test_list',
-    'test_None',
-    'test_NotImplemented',
-    'test_range',
-    'test_set',
-    'test_slice',
-    'test_str',
-    'test_tuple',
-    ]
+    not_implemented = [
+        'test_bool',
+        'test_bytearray',
+        'test_bytes',
+        'test_class',
+        'test_complex',
+        'test_dict',
+        'test_float',
+        'test_frozenset',
+        'test_int',
+        'test_list',
+        'test_None',
+        'test_NotImplemented',
+        'test_range',
+        'test_set',
+        'test_slice',
+        'test_str',
+        'test_tuple',
+        ]
