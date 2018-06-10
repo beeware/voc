f984bd263aff9b3d4c85e8fd3b512dc7e7db99ad
Patience Shyu <patienceshyu@gmail.com>
2018-06-10 22:33:34 +0200

    fix indentation issues

--- python/common/org/python/types/Int.java
+++ python/common/org/python/types/Int.java
@@ -49,11 +49,11 @@ public class Int extends org.python.types.Object {
         if (-NSMALLNEGINTS <= value && value < NSMALLPOSINTS) {
             int index = (int) value + NSMALLNEGINTS;
             org.python.types.Int i_obj = SMALLINTS[index];
-        if (i_obj == null) {
-            i_obj = new org.python.types.Int(value);
-            SMALLINTS[index] = i_obj;
-        }
-        return i_obj;
+            if (i_obj == null) {
+                i_obj = new org.python.types.Int(value);
+                SMALLINTS[index] = i_obj;
+            }
+            return i_obj;
       } else {
           return new org.python.types.Int(value);
       }
