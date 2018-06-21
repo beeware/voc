4bffb3253be831998a5078df5090e1aa92544509
Patience Shyu <patienceshyu@gmail.com>
2018-06-21 08:52:41 +0200

    substitutions should include sample_subs

--- tests/builtins/test_set.py
+++ tests/builtins/test_set.py
@@ -1,4 +1,8 @@
-from .. utils import TranspileTestCase, BuiltinFunctionTestCase
+from .. utils import (
+TranspileTestCase,
+BuiltinFunctionTestCase,
+SAMPLE_SUBSTITUTIONS
+)
 
 
 class SetTests(TranspileTestCase):
@@ -21,25 +25,8 @@ class BuiltinSetFunctionTests(BuiltinFunctionTestCase, TranspileTestCase):
     ]
 
     substitutions = {
-        # output, keyed to all possible inputs
         "{3, 1.2, True}": [
             "{1.2, 3, True}", "{True, 1.2, 3}", "{True, 3, 1.2}", "{3, True, 1.2}", "{1.2, True, 3}"
-        ],
-        "{1, 2.3456, 'another'}": [
-            "{1, 'another', 2.3456}", "{'another', 1, 2.3456}", "{'another', 2.3456, 1}", "{2.3456, 'another', 1}",
-            "{2.3456, 1, 'another'}"
-        ],
-        "{'on', 'to', 'an'}": [
-            "{'on', 'an', 'to'}", "{'to', 'an', 'on'}", "{'to', 'on', 'an'}", "{'an', 'to', 'on'}", "{'an', 'on', 'to'}"
-        ],
-        "{'one', 'two', 'six'}": [
-            "{'one', 'six', 'two'}", "{'two', 'one', 'six'}", "{'two', 'six', 'one'}", "{'six', 'one', 'two'}",
-            "{'six', 'two', 'one'}"
-        ],
-        "{1, 2.3456, 7}": [
-            "{1, 7, 2.3456}", "{2.34556, 1, 7}", "{2.3456, 7, 1}", "{7, 2.3456, 1}", "{7, 1, 2.3456}"
-        ],
-        "{'a', 'b', 'c'}": [
-            "{'a', 'c', 'b'}", "{'b', 'a', 'c'}", "{'b', 'c', 'a'}", "{'c', 'a', 'b'}", "{'c', 'b', 'c'}"
         ]
     }
+    substitutions.update(SAMPLE_SUBSTITUTIONS)
