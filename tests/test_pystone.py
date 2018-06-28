4c65f636888436fa1a1c20e3f13b78ae9a158203
Patience Shyu <patienceshyu@gmail.com>
2018-06-28 11:38:35 +0200

    separate pystone file from test file

---
+++ tests/test_pystone.py
@@ -0,0 +1,16 @@
+import os
+
+from .utils import *
+
+__file__ = "pystone.py"
+__thisdir__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
+__location__ = os.path.join(__thisdir__, "tests/", __file__)
+
+class PystoneTest(TranspileTestCase):
+
+    def test_pystone(self):
+        file = open(__location__, "r")
+
+        out = self.runAsJava(adjust(file.read()))
+
+        file.close()
