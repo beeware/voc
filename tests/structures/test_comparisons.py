09a88804f96726dc19c28e11aa3108ac25f1fb77
Patience Shyu <patienceshyu@gmail.com>
2018-07-27 09:47:15 +0200

    add failing tests for contains/not contains

--- tests/structures/test_comparisons.py
+++ tests/structures/test_comparisons.py
@@ -583,3 +583,12 @@ class ComparisonTests(TranspileTestCase):
             print(x == (50 + 50) < 2000)
             print(x == (50 + 50) > 2000)
             """)
+
+    @expectedFailure
+    def test_bad_in(self):
+        self.assertCodeExecution("""
+            print('abc' in 'abc')
+            print(0 in 0)
+            print(0 not in 0)
+            print('abc' not in 'abc')
+        """)
