f8729555ce08802da87387ac6c6935be496a91d4
Patience Shyu <patienceshyu@gmail.com>
2018-08-20 17:37:14 +0200

    Add new benchmarks

---
+++ tests/benchmarks/unpack_sequence.py
@@ -0,0 +1,154 @@
+#!/usr/bin/env python
+
+"""Microbenchmark for Python's sequence unpacking."""
+
+import time
+
+
+def do_unpacking(iterations, to_unpack):
+    times = []
+    for _ in range(iterations):
+        t0 = time.time()
+        # 400 unpackings
+        a, b, c, d, e, f, g, h, i, j = to_unpack
+        a, b, c, d, e, f, g, h, i, j = to_unpack
+        a, b, c, d, e, f, g, h, i, j = to_unpack
+        a, b, c, d, e, f, g, h, i, j = to_unpack
+        a, b, c, d, e, f, g, h, i, j = to_unpack
+        a, b, c, d, e, f, g, h, i, j = to_unpack
+        a, b, c, d, e, f, g, h, i, j = to_unpack
+        a, b, c, d, e, f, g, h, i, j = to_unpack
+        a, b, c, d, e, f, g, h, i, j = to_unpack
+        a, b, c, d, e, f, g, h, i, j = to_unpack
+        a, b, c, d, e, f, g, h, i, j = to_unpack
+        a, b, c, d, e, f, g, h, i, j = to_unpack
+        a, b, c, d, e, f, g, h, i, j = to_unpack
+        a, b, c, d, e, f, g, h, i, j = to_unpack
+        a, b, c, d, e, f, g, h, i, j = to_unpack
+        a, b, c, d, e, f, g, h, i, j = to_unpack
+        a, b, c, d, e, f, g, h, i, j = to_unpack
+        a, b, c, d, e, f, g, h, i, j = to_unpack
+        a, b, c, d, e, f, g, h, i, j = to_unpack
+        a, b, c, d, e, f, g, h, i, j = to_unpack
+        a, b, c, d, e, f, g, h, i, j = to_unpack
+        a, b, c, d, e, f, g, h, i, j = to_unpack
+        a, b, c, d, e, f, g, h, i, j = to_unpack
+        a, b, c, d, e, f, g, h, i, j = to_unpack
+        a, b, c, d, e, f, g, h, i, j = to_unpack
+        a, b, c, d, e, f, g, h, i, j = to_unpack
+        a, b, c, d, e, f, g, h, i, j = to_unpack
+        a, b, c, d, e, f, g, h, i, j = to_unpack
+        a, b, c, d, e, f, g, h, i, j = to_unpack
+        a, b, c, d, e, f, g, h, i, j = to_unpack
+        a, b, c, d, e, f, g, h, i, j = to_unpack
+        a, b, c, d, e, f, g, h, i, j = to_unpack
+        a, b, c, d, e, f, g, h, i, j = to_unpack
+        a, b, c, d, e, f, g, h, i, j = to_unpack
+        a, b, c, d, e, f, g, h, i, j = to_unpack
+        a, b, c, d, e, f, g, h, i, j = to_unpack
+        a, b, c, d, e, f, g, h, i, j = to_unpack
+        a, b, c, d, e, f, g, h, i, j = to_unpack
+        a, b, c, d, e, f, g, h, i, j = to_unpack
+        a, b, c, d, e, f, g, h, i, j = to_unpack
+        a, b, c, d, e, f, g, h, i, j = to_unpack
+        a, b, c, d, e, f, g, h, i, j = to_unpack
+        a, b, c, d, e, f, g, h, i, j = to_unpack
+        a, b, c, d, e, f, g, h, i, j = to_unpack
+        a, b, c, d, e, f, g, h, i, j = to_unpack
+        a, b, c, d, e, f, g, h, i, j = to_unpack
+        a, b, c, d, e, f, g, h, i, j = to_unpack
+        a, b, c, d, e, f, g, h, i, j = to_unpack
+        a, b, c, d, e, f, g, h, i, j = to_unpack
+        a, b, c, d, e, f, g, h, i, j = to_unpack
+        a, b, c, d, e, f, g, h, i, j = to_unpack
+        a, b, c, d, e, f, g, h, i, j = to_unpack
+        a, b, c, d, e, f, g, h, i, j = to_unpack
+        a, b, c, d, e, f, g, h, i, j = to_unpack
+        a, b, c, d, e, f, g, h, i, j = to_unpack
+        a, b, c, d, e, f, g, h, i, j = to_unpack
+        a, b, c, d, e, f, g, h, i, j = to_unpack
+        a, b, c, d, e, f, g, h, i, j = to_unpack
+        a, b, c, d, e, f, g, h, i, j = to_unpack
+        a, b, c, d, e, f, g, h, i, j = to_unpack
+        a, b, c, d, e, f, g, h, i, j = to_unpack
+        a, b, c, d, e, f, g, h, i, j = to_unpack
+        a, b, c, d, e, f, g, h, i, j = to_unpack
+        a, b, c, d, e, f, g, h, i, j = to_unpack
+        a, b, c, d, e, f, g, h, i, j = to_unpack
+        a, b, c, d, e, f, g, h, i, j = to_unpack
+        a, b, c, d, e, f, g, h, i, j = to_unpack
+        a, b, c, d, e, f, g, h, i, j = to_unpack
+        a, b, c, d, e, f, g, h, i, j = to_unpack
+        a, b, c, d, e, f, g, h, i, j = to_unpack
+        a, b, c, d, e, f, g, h, i, j = to_unpack
+        a, b, c, d, e, f, g, h, i, j = to_unpack
+        a, b, c, d, e, f, g, h, i, j = to_unpack
+        a, b, c, d, e, f, g, h, i, j = to_unpack
+        a, b, c, d, e, f, g, h, i, j = to_unpack
+        a, b, c, d, e, f, g, h, i, j = to_unpack
+        a, b, c, d, e, f, g, h, i, j = to_unpack
+        a, b, c, d, e, f, g, h, i, j = to_unpack
+        a, b, c, d, e, f, g, h, i, j = to_unpack
+        a, b, c, d, e, f, g, h, i, j = to_unpack
+        a, b, c, d, e, f, g, h, i, j = to_unpack
+        a, b, c, d, e, f, g, h, i, j = to_unpack
+        a, b, c, d, e, f, g, h, i, j = to_unpack
+        a, b, c, d, e, f, g, h, i, j = to_unpack
+        a, b, c, d, e, f, g, h, i, j = to_unpack
+        a, b, c, d, e, f, g, h, i, j = to_unpack
+        a, b, c, d, e, f, g, h, i, j = to_unpack
+        a, b, c, d, e, f, g, h, i, j = to_unpack
+        a, b, c, d, e, f, g, h, i, j = to_unpack
+        a, b, c, d, e, f, g, h, i, j = to_unpack
+        a, b, c, d, e, f, g, h, i, j = to_unpack
+        a, b, c, d, e, f, g, h, i, j = to_unpack
+        a, b, c, d, e, f, g, h, i, j = to_unpack
+        a, b, c, d, e, f, g, h, i, j = to_unpack
+        a, b, c, d, e, f, g, h, i, j = to_unpack
+        a, b, c, d, e, f, g, h, i, j = to_unpack
+        a, b, c, d, e, f, g, h, i, j = to_unpack
+        a, b, c, d, e, f, g, h, i, j = to_unpack
+        a, b, c, d, e, f, g, h, i, j = to_unpack
+        a, b, c, d, e, f, g, h, i, j = to_unpack
+        a, b, c, d, e, f, g, h, i, j = to_unpack
+        a, b, c, d, e, f, g, h, i, j = to_unpack
+        a, b, c, d, e, f, g, h, i, j = to_unpack
+        a, b, c, d, e, f, g, h, i, j = to_unpack
+        a, b, c, d, e, f, g, h, i, j = to_unpack
+        a, b, c, d, e, f, g, h, i, j = to_unpack
+        a, b, c, d, e, f, g, h, i, j = to_unpack
+        a, b, c, d, e, f, g, h, i, j = to_unpack
+        a, b, c, d, e, f, g, h, i, j = to_unpack
+        a, b, c, d, e, f, g, h, i, j = to_unpack
+        a, b, c, d, e, f, g, h, i, j = to_unpack
+        a, b, c, d, e, f, g, h, i, j = to_unpack
+        a, b, c, d, e, f, g, h, i, j = to_unpack
+        a, b, c, d, e, f, g, h, i, j = to_unpack
+        t1 = time.time()
+        times.append(t1 - t0)
+    return times
+
+
+def test_tuple_unpacking(iterations):
+    x = tuple(range(10))
+    return do_unpacking(iterations, x)
+
+
+def test_list_unpacking(iterations):
+    x = range(10)
+
+    return do_unpacking(iterations, x)
+
+
+def test_all(iterations):
+    tuple_data = test_tuple_unpacking(iterations)
+    list_data = test_list_unpacking(iterations)
+
+    return [x + y for (x, y) in zip(tuple_data, list_data)]
+
+if __name__ == "__main__":
+    import sys
+    loops = int(sys.argv[1])
+    times = test_all(loops)
+
+    print("Time elapsed: " + str(sum(times)) + " sec")
